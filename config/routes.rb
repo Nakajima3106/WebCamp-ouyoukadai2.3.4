723475c97a70619d6ca729867a4a4f47a9d16969
Nakajima3106 <uver777.333@gmail.com>
2020-12-05 08:16:19 +0000

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get "home/about" => "homes#about"
   
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
+    
   end
   resources :users, only: [:index, :show, :edit, :update]
+  resource :relationships, only: [:create, :destroy]
 end
