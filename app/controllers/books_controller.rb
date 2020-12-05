c1a7f4dc9179ebcf84ae19ff0e0688da013149f2
Nakajima3106 <uver777.333@gmail.com>
2020-12-05 04:40:47 +0000

    いいね、コメント機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
@@ -48,6 +48,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
