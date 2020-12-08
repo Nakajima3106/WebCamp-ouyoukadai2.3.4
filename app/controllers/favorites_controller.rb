aaf894a8272a33c584f2fc6849702e2c35c4da49
Nakajima3106 <uver777.333@gmail.com>
2020-12-08 06:21:27 +0000

    非同期通信実装終了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
@@ -12,5 +12,5 @@ class FavoritesController < ApplicationController
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
   end
-  
+
 end
