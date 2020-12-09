bf4bacab9d27ca3f02d3ed065a627e76a0cd6a11
Nakajima3106 <uver777.333@gmail.com>
2020-12-09 04:52:25 +0000

    コメント機能の非同期通信の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,18 +6,14 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to request.referer
-		else
-		  render 'books/show'
+		unless @book_comment.save
 		end
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
-  	book_comment = @book.book_comments.find(params[:id])
+  	    book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
 	end
 
 	private
